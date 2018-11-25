198b5e43813571fe031d12ca1dc5ceb1ed302f0f
zhen-ke <cool420984@gmail.com>
2018-11-25 20:58:39 +0800

    核心逻辑重写与接口更新

--- pages/index/index.js
+++ pages/index/index.js
@@ -60,23 +60,8 @@ Page({
     city: "上地",
     summary: "多云",
     localTemperature: "1",
-    days: [{
-      time: "今天",
-      icon: "./image/100.svg",
-      detail: "多云",
-      minTemperature: "10",
-      maxTemperature: "16",
-    }, ],
-    suggestion: {
-      air: "良", // 空气指数
-      comf: "良", // 舒适度指数
-      cw: "良", // 洗车指数
-      drsg: "良", // 穿衣指数
-      flu: "良", // 感冒指数
-      sport: "良", // 运动指数
-      trav: "良", // 旅游指数
-      uv: "良", // 紫外线指数
-    },
+    days: [],
+    suggestion: [],
     suggestionIcon: {
       air: "./image/life/air.svg",
       cw: "./image/life/cw.svg",
@@ -87,13 +72,7 @@ Page({
       trav: "./image/life/trav.svg",
       comf: "./image/life/comf.svg",
     },
-    detail: {
-      windSpeed: "1",
-      windy: "1",
-      temperature: "1",
-      barometer: "1",
-      humidity: "1",
-    },
+    detail: {},
     detailIcon: {
       windy: "./image/detail/windy.svg",
       barometer: "./image/detail/barometer.svg",
@@ -104,129 +83,152 @@ Page({
     prompt: "Loading ...", // 页面的初始数据
     lodingsrc: "./image/location/umbrella.svg",
     air: {
-      aqi: '',
-      co: '',
-      no2: '',
-      o3: '',
-      pm10: '',
-      pm25: '',
-      qlty: '',
-      so2: '',
+      aqi: 'AQI',
+      co: '一氧化碳',
+      no2: '二氧化氮',
+      o3: '臭氧',
+      pm10: 'PM10',
+      pm25: 'PM2.5',
+      qlty: '空气质量',
+      so2: '二氧化硫',
     }
   },
-  getUserLocation: function() { // 获取用户当前经纬度
-    let self = this
+  getUserLocation() { // 获取用户当前经纬度
     wx.getLocation({
       type: 'wgs84',
-      success: function(res) {
-        self.decodingGps(res.longitude,res.latitude)
+      success: (res) => {
+        this.decodingGps(res.longitude, res.latitude)
       },
-      fail: function() {
-        self.add()
+      fail: () => {
+        this.add()
       }
     })
   },
-  decodingGps: function(x,y) { // 解析经纬度到到地址
-    let self = this
+  decodingGps(x, y) { // 解析经纬度到到地址
     wx.request({
       url: 'https://restapi.amap.com/v3/geocode/regeo',
       data: {
         location: x + "," + y,
         key: '6bcab73e18c29692d1678e027e7be25a',
       },
-      header: {'content-type': 'application/json'},
-      success: function(res) {
-        console.log(res.data.regeocode.addressComponent.district)
-        self.setData({
+      header: { 'content-type': 'application/json' },
+      success: (res) => {
+        this.setData({
           location: res.data.regeocode.addressComponent.district
         })
-        self.getWeather()
+        this.getWeather()
+        this.getNowWeather()
+        this.getLifestyle()
+        // this.getAir()
       },
-      fail: function() {
-        self.add()
+      fail: () => {
+        this.add()
       }
     })
   },
-  getWeather: function() { // 获取并解析天气
-    let self = this
+  getNowWeather() { // 实况天气
     wx.request({
-      url: 'https://free-api.heweather.com/v5/weather',
-      data: {city: self.data.location, key: '01a7798b060b468abdad006ea3de4713'},
-      header: {'content-type': 'application/json'},
-      success: function(res) {
-        // console.log(res)
-        let detailTemp = {}
-        let moreDaysTemp = {}
-        let suggestionTemp = {}
-        let moreDaysMap = ["星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六"]
-        let airTemp = {}
-        airTemp = { // 空气质量
-          aqi: res.data.HeWeather5[0].aqi.city.aqi,
-          co: res.data.HeWeather5[0].aqi.city.co,
-          no2: res.data.HeWeather5[0].aqi.city.no2,
-          o3: res.data.HeWeather5[0].aqi.city.o3,
-          pm10: res.data.HeWeather5[0].aqi.city.pm10,
-          pm25: res.data.HeWeather5[0].aqi.city.pm25,
-          qlty: res.data.HeWeather5[0].aqi.city.qlty,
-          so2: res.data.HeWeather5[0].aqi.city.so2,
-        }
-        detailTemp = { // 实况天气
-          windSpeed: ((res.data.HeWeather5[0].now.wind.spd * 0.278) / 0.621).toFixed(0),
-          windy: res.data.HeWeather5[0].now.wind.dir,
-          temperature: res.data.HeWeather5[0].now.fl,
-          barometer: res.data.HeWeather5[0].now.pres,
-          humidity: res.data.HeWeather5[0].now.hum,
-        }
-        for (let i = 1; i < res.data.HeWeather5[0].daily_forecast.length - 1; i++) { // 天气预报forecast
-          moreDaysTemp[i] = {
-            time: moreDaysMap[new Date(res.data.HeWeather5[0].daily_forecast[i].date.split("-").join("/")).getDay()],
-            icon: self.data.conditionCode[res.data.HeWeather5[0].daily_forecast[i].cond.code_d],
-            detail: res.data.HeWeather5[0].daily_forecast[i].cond.txt_d,
-            minTemperature: res.data.HeWeather5[0].daily_forecast[i].tmp.min,
-            maxTemperature: res.data.HeWeather5[0].daily_forecast[i].tmp.max
-          }
-        }
-        suggestionTemp = { // 生活指数
-          air: res.data.HeWeather5[0].suggestion.air.brf,
-          comf: res.data.HeWeather5[0].suggestion.comf.brf,
-          cw: res.data.HeWeather5[0].suggestion.cw.brf,
-          drsg: res.data.HeWeather5[0].suggestion.drsg.brf,
-          flu: res.data.HeWeather5[0].suggestion.flu.brf,
-          sport: res.data.HeWeather5[0].suggestion.sport.brf,
-          trav: res.data.HeWeather5[0].suggestion.trav.brf,
-          uv: res.data.HeWeather5[0].suggestion.uv.brf,
-        }
-        self.setData({ // 更新数据
-          city: res.data.HeWeather5[0].basic.city,
-          summary: res.data.HeWeather5[0].now.cond.txt,
-          localTemperature: res.data.HeWeather5[0].now.tmp,
-          detail: detailTemp,
-          suggestion: suggestionTemp,
-          days: moreDaysTemp,
-          air: airTemp,
+      url: 'https://free-api.heweather.com/s6/weather/now',
+      data: {
+        location: this.data.location,
+        key: 'e4f463c603ec41628d4d497b5eccbe6a'
+      },
+      header: { 'content-type': 'application/json' },
+      success: (res) => {
+        let { basic, now } = res.data.HeWeather6[0]
+        this.setData({
+          city: basic.parent_city,
+          summary: now.cond_txt,
+          localTemperature: now.tmp,
+          detail: now
+        })
+      },
+      fail: () => {
+        this.add()
+      }
+    })
+  },
+  getLifestyle() { // 生活指数
+    wx.request({
+      url: 'https://free-api.heweather.com/s6/weather/lifestyle',
+      data: {
+        location: this.data.location,
+        key: 'e4f463c603ec41628d4d497b5eccbe6a'
+      },
+      header: { 'content-type': 'application/json' },
+      success: (res) => {
+        let suggestion = res.data.HeWeather6[0].lifestyle
+        this.setData({
+          suggestion
+        })
+      },
+      fail: () => {
+        this.add()
+      }
+    })
+  },
+  getAir() { // 空气质量实况
+    wx.request({
+      url: 'https://free-api.heweather.com/s6/air/now',
+      data: {
+        location: this.data.location,
+        key: 'e4f463c603ec41628d4d497b5eccbe6a'
+      },
+      header: { 'content-type': 'application/json' },
+      success: (res) => {
+        let suggestion = res.data.HeWeather6[0].lifestyle
+        this.setData({
+          suggestion
+        })
+      },
+      fail: () => {
+        this.add()
+      }
+    })
+  },
+  getWeather: function () { // 3-10天天气预报
+    wx.request({
+      url: 'https://free-api.heweather.com/s6/weather/forecast',
+      data: {
+        location: this.data.location,
+        key: 'e4f463c603ec41628d4d497b5eccbe6a'
+      },
+      header: { 'content-type': 'application/json' },
+      success: (res) => {
+        let arrayWeather = res.data.HeWeather6[0].daily_forecast.slice(1, 6)
+        let weekDaysMap = ["星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六"]
+        let list = arrayWeather.map(it => ({
+          time: weekDaysMap[new Date(it.date.split("-").join("/")).getDay()],
+          icon: this.data.conditionCode[it.cond_code_d],
+          detail: it.cond_txt_d,
+          minTemperature: it.tmp_min,
+          maxTemperature: it.tmp_max
+        }))
+        this.setData({
+          days: list,
           show: false,
         })
       },
-      fail: function() {
-        self.add()
+      fail: () => {
+        this.add()
       }
     })
   },
-  onPullDownRefresh: function() { // 页面相关事件处理函数--监听用户下拉动作
-    setTimeout(function() {
+  onPullDownRefresh: function () { // 页面相关事件处理函数--监听用户下拉动作
+    setTimeout(function () {
       wx.stopPullDownRefresh()
-    },1000)
-    if(!this.data.show){
+    }, 1000)
+    if (!this.data.show) {
       this.getUserLocation()
     }
   },
-  onLoad: function() { // 生命周期函数--监听页面加载
+  onLoad: function () { // 生命周期函数--监听页面加载
     this.getUserLocation()
     wx.showShareMenu({ // 转发
       withShareTicket: true
     })
   },
-  onShow: function() { // 生命周期函数--监听页面显示
+  onShow: function () { // 生命周期函数--监听页面显示
     let dataFromSearch = wx.getStorageSync('data');
     if (dataFromSearch) {
       this.setData({
@@ -237,10 +239,13 @@ Page({
         this.getUserLocation()
       } else if (this.data.location) {
         this.getWeather()
+        this.getNowWeather()
+        this.getLifestyle()
+        // this.getAir()　
       }
     }
   },
-  add: function() { // 转跳到搜索页面
+  add: function () { // 转跳到搜索页面
     wx.navigateTo({
       url: '/pages/search/index'
     })
